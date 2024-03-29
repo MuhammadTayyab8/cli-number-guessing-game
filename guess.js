de9b43152d446051672fc103489efff94e784015
Muhammad Tayyab <160164154+MuhammadTayyab8@users.noreply.github.com>
2024-03-29 16:58:58 +0500

    open

---
+++ guess.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let attemptsLeft = 5; // You can adjust the number of attempts as needed
+let secretNumber = Math.floor(Math.random() * 30 + 1);
+let correct = false;
+async function playGame() {
+    console.log(chalk.yellow("\nWelcome to the Guessing Game!\n"));
+    console.log(chalk.greenBright(`Guess the secret number between 1 and 30. You have ${attemptsLeft} attempts.\n`));
+    while ((attemptsLeft > 0) && (!correct)) {
+        const guess = await inquirer.prompt({
+            name: "number",
+            type: "number",
+            message: `Attempts left: ${attemptsLeft}. Enter your guess:`,
+        });
+        if (guess.number === secretNumber) {
+            console.log(chalk.green("Congratulations! You guessed the secret number!"));
+            correct = true;
+        }
+        else if (guess.number < secretNumber) {
+            console.log(chalk.blue("Too low. Try again.\n"));
+        }
+        else if (guess.number > secretNumber) {
+            console.log(chalk.yellow("Too high. Try again.\n"));
+        }
+        else {
+            console.log("Please enter a valid number.\n");
+        }
+        attemptsLeft--;
+    }
+    // If the user fails to guess the number within attempts
+    if (!correct) {
+        console.log(chalk.red("Sorry, you've run out of attempts. The secret number was:", secretNumber));
+    }
+}
+// Start the game
+playGame();
